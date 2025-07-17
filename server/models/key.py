a3ccf5ce93c74993cec9b0776228c6fd8773e23f
Philip Magyar <philipandremagyar@gmail.com>
2025-07-17 22:59:32 +0200

    the label should be on the key not tag, maybe rename the one on tag to desc and also make that optional

--- server/models/key.py
+++ server/models/key.py
@@ -5,9 +5,10 @@ class Key(db.Model):
     __tablename__ = 'key'
 
     private_key = db.Column(db.String, nullable=False)
-    public_key = db.Column(db.String, primary_key=True)
+    public_key = db.Column(db.String, nullable=False, primary_key=True)
     hashed_public_key = db.Column(db.String, nullable=False)
-    tag_id = db.Column(db.String, db.ForeignKey('tag.id', ondelete='CASCADE'), nullable=True)
+    label = db.Column(db.String, nullable=True)
+    tag_id = db.Column(db.String, db.ForeignKey('tag.id', ondelete='CASCADE'), nullable=False)
     created_at = db.Column(db.Integer, default=lambda: int(time.time()), nullable=False)
 
     def __init__(self, private_key, public_key, hashed_public_key, tag_id=None):
@@ -20,6 +21,7 @@ class Key(db.Model):
         return {
             'public_key': self.public_key,
             'hashed_public_key': self.hashed_public_key,
+            'label': self.label,
             'tag_id': self.tag_id
         }
         
